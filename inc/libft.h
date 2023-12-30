a6996ed4d7b22dcaac2f6b68dae0f7215980890f
Mahmut Demir <103860270+mahdemir@users.noreply.github.com>
2023-12-30 07:51:10 +0100

    improved ft_matrix_free func

--- inc/libft.h
+++ inc/libft.h
@@ -6,7 +6,7 @@
 /*   By: mademir <mademir@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/02 17:05:53 by mademir       #+#    #+#                 */
-/*   Updated: 2023/12/30 07:45:53 by mademir       ########   odam.nl         */
+/*   Updated: 2023/12/30 07:50:34 by mademir       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,7 +121,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 
 /* Free complete matrix */
-void	ft_matrix_free(void **arr);
+void	ft_matrix_free(void **matrix);
 
 /* Locates the first occurrence of c in s. */
 void	*ft_memchr(const void *s, int c, size_t n);
